17ef7bc6dcfdfda3c66cc6e42c7802adfea6444f
Aidan Ruiz <send2aidan@yahoo.com>
2023-08-13 12:36:43 -0400

    SQL and Django

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("tasks/", include("tasks.urls")),
     path("admin/", admin.site.urls),
     path("hello/", include("hello.urls")),
     path("newyear/", include("newyear.urls")),
